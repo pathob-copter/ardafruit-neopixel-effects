f036303d53153857d047b1e6e0f2fd7c37961d5b
Patrick Hobusch <patrick.hobusch@gmail.com>
2015-11-15 00:27:12 +0100

    Smooth continous dots changing color.

--- ws2812b-control.ino
+++ ws2812b-control.ino
@@ -3,19 +3,25 @@
 #define LED_01_PIN      3
 #define LED_01_NUM      13
 
+#define STEPS_NUM       7
+
 #define RED   0
 #define GREEN 1
 #define BLUE  2
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(LED_01_NUM, LED_01_PIN, NEO_GRB + NEO_KHZ800);
 
-int delayval = 40; // delay for half a second
+int delayval = 80; // delay for half a second
 
 bool increase = true;
 int index = 0;
 
+int step = 0;
+
 int color[3];
 int offset;
+
+int divisor[STEPS_NUM] = {20, 1, 3, 8, 20, 100, 255};
 
 void setup() {
   Serial.begin(9600);
@@ -33,16 +39,19 @@ void setup() {
 
 void loop() {
   
-  for(int i=0;i<LED_01_NUM;i++){
-    pixels.setPixelColor(i, pixels.Color(0,0,0));
+  for(int i = LED_01_NUM; i > 0; i--) {
+    uint32_t nextLedColor = pixels.getPixelColor(i - 1);
+    pixels.setPixelColor(i, nextLedColor);
   }
+
+  color[offset]--;
+  color[(offset+1)%3]++;
   
   if (color[offset] == 0) {
     offset = (offset + 1) % 3;
   }
   
-  color[offset]--;
-  color[(offset+1)%3]++;
+  pixels.setPixelColor(0, pixels.Color(color[RED] / divisor[step], color[GREEN] / divisor[step], color[BLUE] / divisor[step]));
   
   Serial.print(color[RED]);
   Serial.print(" - ");
@@ -50,8 +59,7 @@ void loop() {
   Serial.print(" - ");
   Serial.println(color[BLUE]);
   
-  pixels.setPixelColor(index, pixels.Color(color[RED],color[GREEN],color[BLUE]));
-  
+  step = (step + 1) % STEPS_NUM;
   index = (index + 1) % LED_01_NUM;
   
   pixels.show();
